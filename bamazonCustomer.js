6c45a6ace5f33c8fcc7f11273d5f02f9052ed005
violettebec <bechoerner@gmail.com>
2019-03-08 20:30:13 -0600

    fixing issue with inquirer prompt array

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,5 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-// var setMaxListeners = require('events').EventEmitter.prototype._maxListeners = 100;
 
 
 // create the connection information for the sql database     
@@ -14,7 +13,6 @@ var connection = mysql.createConnection({
 
 // connecting to the mysql server and sql database
 connection.connect(function (err) {
-    // setMaxListeners;
     if (err) throw err;
     console.log('CONNECTION STATUS: Connection successful');
     console.log('----------------------------------------------------------')
@@ -26,7 +24,6 @@ connection.connect(function (err) {
 //The first should ask them the ID of the product they would like to buy.
 //The second message should ask how many units of the product they would like to buy.
 function start() {
-    // setMaxListeners;
     //start with a greeting 
     console.log("Welcome to Bamazon, our online pet supply store!");
 
@@ -38,15 +35,13 @@ function start() {
     });
 
     function promptUser() {
-        // setMaxListeners;
         inquirer
-            .prompt({
+            .prompt([{
                 name: "whichProduct",
                 type: "input",
                 message: "What is the ID of the product you would like to buy?",
                 //check to ensure the entered value is a number
                 validate: function (value) {
-                    console.log("here is our value:" + value);
                     if (isNaN(value) === false) {
                         return true;
                     }
@@ -58,29 +53,30 @@ function start() {
                 message: "How many units of the product would you like to buy? Input a quantity",
                 validate: function (value) {
                     if (isNaN(value) === false) {
-                        console.log('here is the quantity: ', value)
                         return true;
                     }
                     return "please enter a number 1-50!";
                 }
-            })
+            }])
 
             // based on the user answer, call the corresponding whichProduct & whatQuantity 
             .then(answer => {
-                // console.log('here is the answer: ', answer)
                 connection.query("SELECT * FROM products WHERE ?", {
                         id: answer.whichProduct
                     },
                     function (err, res) {
                         if (err) throw (err);
-                        // console.log("here is the res: ", res)
 
                         //if the requested quantity is less than the available stock, fill the order
                         if (answer.whatQuantity <= res[0].stock_quantity) {
+                            console.log('----------------------------------------------------------')
+                            console.log("You have added " + answer.whatQuantity + " " + res[0].product_name + " to your order!")
+                            console.log('----------------------------------------------------------')
                             console.log("Confirming your order!");
 
                             //call our dbUpdate function to update our bamazonDB database
                             dbUpdate(answer.whichProduct, answer.whatQuantity, res[0].stock_quantity, res[0].price);
+
 
                             //if the inventory is not sufficient, log a message for the user
                         } else {
@@ -107,7 +103,8 @@ function start() {
             }
         )
         //log the cost for the user
-        console.log("Your total cost is: " + price);
+        console.log("Your total cost is: " + price * requestedQuantity);
+        console.log('----------------------------------------------------------')
         complete();
     }
 
